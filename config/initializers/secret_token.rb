cd065befaa59a06a84c926fdfe49f9c1f79d47f0
Nguyen Khanh Hung <khanhhungnguyen@gmail.com>
2011-07-18 14:38:26 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLab::Application.config.secret_token = 'a86dddfbb76115403c66c409ea1f2ab6e6f2ff3aab673117737b16f3f1445baf7b221e9fb4f6bd15a4e56d2af0a1e9c923a19ecf71b3855e377bd309271412ba'
